1b13ddca603c370d04199755532abf2dfd87a58c
Keerthivasan Kumar <Keerthik1407@gmail.com>
2021-03-30 17:29:22 -0400

    Test case-4 under 03-product.js has been tested successfully

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -27,25 +27,29 @@ try {
   // Test Case 1
   // --------------------------------------------------
   // It should return the product of two numbers.
-  var result = product(1, 2);
+var result = product(1, 2);
   if (result !== 2) throw new Error('Expected product(1, 2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
-   
+
+var result = product(1, 0);
+ if (result !== 0) throw new Error('Expected product(1, 0) to be 0. Received: ' + result);
  //---------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
- 
+ var result = product(1, 2, 3, 4, 5, 6, 7, 8);
+	  if (result !== 2) throw new Error('Expected product(1, 2, 3, 4, 5, 6, 7, 8) to be 2. Received: ' + result);
  //----------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
-  // --------------------------------------------------
+ var result = product(1, null);
+	  if (result !== 1) throw new Error('Expected product(1, null) to be 1. Received: ' + result);
+// --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
